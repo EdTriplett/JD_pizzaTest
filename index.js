ebf6cbb985253c862ab702647584371414e4f2fc
EdTriplett <EdTriplett@users.noreply.github.com>
2018-01-30 23:14:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import fetch from 'isomorphic-fetch';
+
+// Note: this is the entry point for your application
+
+// step 1: you will need to load the pizza data. it is available at /pizza.json. what-wg fetch is pre-installed.
+// remember that fetch uses promises.
+
+// step 2: implement the view and required behaviors to pass all the scenarios listed in the readme.
